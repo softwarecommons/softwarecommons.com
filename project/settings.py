79310fc7531571bab8d0403fc986364236af878d
Chad Whitacre <chadwhitacre@sentry.io>
2024-02-28 13:44:05 -0500

    Take DATABASE_URL from environment

--- project/settings.py
+++ project/settings.py
@@ -72,12 +72,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+
+DATABASES = dict(default=dj_database_url.config())
 
 
 # Password validation
