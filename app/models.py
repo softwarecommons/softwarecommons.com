302764cf4d2c6354e6e9418706418e0a6587e766
Chad Whitacre <chadwhitacre@sentry.io>
2024-02-29 11:36:21 -0500

    More model evolution

--- app/models.py
+++ app/models.py
@@ -5,21 +5,32 @@ class Year(models.Model):
     year = models.IntegerField()
     fair_share = models.IntegerField()
 
+    def __str__(self):
+       return str(self.year)
+
 class Organization(models.Model):
     name = models.CharField(max_length=256)
-    ndevs = models.IntegerField()
     img_src = models.URLField(default='')
+
+    def __str__(self):
+       return self.name
 
 class FairShare(models.Model):
     year = models.ForeignKey(Year, on_delete=models.CASCADE)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    amount = models.IntegerField()
+    ndevs = models.IntegerField(null=True)
     paid = models.IntegerField(default=0)
+
+    def __str__(self):
+       return str(self.year)
 
 class OpenSourceEcosystem(models.Model):
     name = models.CharField(max_length=256)
     owner = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True)
     img_src = models.URLField(default='')
+
+    def __str__(self):
+       return self.name
 
 class OpenSourceProject(models.Model):
     name = models.CharField(max_length=256)
@@ -27,11 +38,20 @@ class OpenSourceProject(models.Model):
     ecosystem = models.ForeignKey(OpenSourceEcosystem, on_delete=models.CASCADE)
     img_src = models.URLField(default='')
 
+    def __str__(self):
+       return self.name
+
 class OpenProductModel(models.Model):
     name = models.CharField(max_length=256)
+
+    def __str__(self):
+       return self.name
 
 class OpenProduct(models.Model):
     name = models.CharField(max_length=256)
     model = models.ForeignKey(OpenProductModel, on_delete=models.CASCADE)
     owner = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True)
     img_src = models.URLField(default='')
+
+    def __str__(self):
+       return self.name
